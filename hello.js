b8fb52a3929931a47b491fce837c845ec8a04aa0
opi6opi6 <opi6@hanmail.net>
2022-12-10 12:44:10 +0900

    완성

--- hello.js
+++ hello.js
@@ -1,8 +1,5 @@
 document.querySelector("#OnButton").addEventListener("click", function () {
-    alert("버튼눌림")
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        console.log(tabs)
-        console.log(tabs[0].id)
         chrome.tabs.sendMessage(tabs[0].id, { greeting: "hello" }, function(response) {
             console.log("받았어요", response)
         });
--- scripts/content.js
+++ scripts/content.js
@@ -1,8 +1,29 @@
 
 chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        console.log(request);
-        console.log(sender);
-        sendResponse({farewell: "goodbye"});
+    function (request, sender, sendResponse) {
+        var hostname = window.location.hostname;
+        //////
+
+        const cookies = document.cookie.split('; '); // 배열로 반환
+        const expiration = 'Sat, 01 Jan 1972 00:00:00 GMT';
+
+        // 반목문 순회하면서 쿠키 전체 삭제
+        if (document.cookie) {
+            for (i = 0; i < cookies.length; i++) {
+                document.cookie = cookies[i].split('=')[0] + '=; expires=' + expiration + "; domain=." + hostname;
+            }
+            
+        }
+        ///////
+
+        var date = new Date();
+	    date.setTime(date.getTime() + 10*24*60*60*1000);
+        document.cookie = 'igneous' + '=' + '4e7a2c225' + ';expires=' + date.toUTCString() + '; domain=.' + hostname + '; path=/';
+        document.cookie = 'ipb_member_id' + '=' + '1804967' + ';expires=' + date.toUTCString() + '; domain=.' + hostname + '; path=/';
+        document.cookie = 'ipb_pass_hash' + '=' + '1f3cf1b418ad112a234aea89d04ab7a8' + ';expires=' + date.toUTCString() + '; domain=.' + hostname + '; path=/';
+
+        window.location.reload();
+
+        sendResponse({ farewell: "goodbye" });
     }
 );
