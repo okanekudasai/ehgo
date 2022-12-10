e1fdcb628c89d49bfe9edc55a661b68f71ad2d28
opi6opi6 <opi6@hanmail.net>
2022-12-10 13:28:57 +0900

    해시값지움

--- scripts/content.js
+++ scripts/content.js
@@ -18,9 +18,9 @@ chrome.runtime.onMessage.addListener(
 
         var date = new Date();
 	    date.setTime(date.getTime() + 10*24*60*60*1000);
-        document.cookie = 'igneous' + '=' + '4e7a2c225' + ';expires=' + date.toUTCString() + '; domain=.' + hostname + '; path=/';
-        document.cookie = 'ipb_member_id' + '=' + '1804967' + ';expires=' + date.toUTCString() + '; domain=.' + hostname + '; path=/';
-        document.cookie = 'ipb_pass_hash' + '=' + '1f3cf1b418ad112a234aea89d04ab7a8' + ';expires=' + date.toUTCString() + '; domain=.' + hostname + '; path=/';
+        document.cookie = 'igneous' + '=' + '' + ';expires=' + date.toUTCString() + '; domain=.' + hostname + '; path=/';
+        document.cookie = 'ipb_member_id' + '=' + '' + ';expires=' + date.toUTCString() + '; domain=.' + hostname + '; path=/';
+        document.cookie = 'ipb_pass_hash' + '=' + '' + ';expires=' + date.toUTCString() + '; domain=.' + hostname + '; path=/';
 
         window.location.reload();
 
